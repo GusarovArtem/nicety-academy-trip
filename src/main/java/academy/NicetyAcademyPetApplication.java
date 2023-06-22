b598c986768a1e85b53777321e01c37ec8225c18
GusarovArtem <artemmaksimovich1@gmail.com>
2023-06-23 00:02:33 +0300

    initial commit

---
+++ src/main/java/academy/NicetyAcademyPetApplication.java
@@ -0,0 +1,13 @@
+package academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicetyAcademyPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NicetyAcademyPetApplication.class, args);
+    }
+
+}
