b9f9acecac2fce960fa128b0153ef9111c705eca
semenovfilipp <znexiss@gmail.com>
2023-07-04 09:40:53 +0300

    [front] - added Header component

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,10 +1,12 @@
 import './styles/App.css';
 import GetStarted from "./components/get-started/GetStarted";
 import logo from './assets/logo.svg';
+import Header from "./components/Header";
 
 function App() {
     return (
         <div className="App">
+            <Header/>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <GetStarted/>
