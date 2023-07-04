317049f3245185938da55fbd06ff3ce959b0e82e
semenovfilipp <znexiss@gmail.com>
2023-07-04 20:32:06 +0300

    [front] - updated navbar

--- src/client/src/components/Navbar.js
+++ src/client/src/components/Navbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#e3f2fd' }}>
+        <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#e3f2fd'}}>
             <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03"
+                    aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarColor03">
+            <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
                         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
@@ -23,10 +24,18 @@ function Navbar() {
                         <a className="nav-link" href="#">About</a>
                     </li>
                 </ul>
-                <form className="form-inline">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-                </form>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link btn"
+                           style={{backgroundColor: "#FBD851", color: "black", marginRight: "10px"}} href="#">Sign
+                            Up</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link btn" style={{backgroundColor: "white", color: "black"}} href="#">Log
+                            In</a>
+                    </li>
+
+                </ul>
             </div>
         </nav>
     );
